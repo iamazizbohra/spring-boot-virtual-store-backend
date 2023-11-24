6c31c0a99894b98f2a70bf42f124452a0e58ff41
Aziz Bohra <imazizbohra@gmail.com>
2023-11-24 14:38:08 +0530

    add products table migration

---
+++ src/main/resources/db/migration/V8__Create_Products_Table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE products (
+	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	store_id BIGINT NOT NULL,
+	name varchar(50) NOT NULL,
+	description varchar,
+	image varchar(255),
+	price INT NOT NULL,
+	old_price INT,
+	quantity INT NOT NULL,
+	enabled boolean,
+	created_by varchar(50),
+	created_date timestamp,
+	last_modified_by varchar(50),
+	last_modified_date timestamp,
+	CONSTRAINT fk_store_id FOREIGN KEY(store_id) REFERENCES stores(id) ON DELETE CASCADE
+);
