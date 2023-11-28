e7d5743863097366a890dd00e56ff2755f8d6949
Aziz Bohra <imazizbohra@gmail.com>
2023-11-28 15:52:07 +0530

    add OrderStatus enum & converter

---
+++ src/main/java/com/coedmaster/vstore/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.coedmaster.vstore.enums;
+
+public enum OrderStatus {
+	PENDING("PENDING"), ACCEPTED("ACCEPTED"), SHIPPED("SHIPPED"), DELIVERED("DELIVERED"), REJECTED("REJECTED"),
+	CANCELLED("CANCELLED");
+
+	OrderStatus(String string) {
+
+	}
+}
