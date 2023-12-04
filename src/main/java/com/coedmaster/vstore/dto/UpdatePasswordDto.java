fd74379775c589432338ab2cb0b01de3056d86de
Aziz Bohra <imazizbohra@gmail.com>
2023-12-04 15:23:14 +0530

    refactor code

--- src/main/java/com/coedmaster/vstore/dto/UpdatePasswordDto.java
+++ src/main/java/com/coedmaster/vstore/dto/UpdatePasswordDto.java
@@ -1,11 +1,9 @@
 package com.coedmaster.vstore.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class UpdatePasswordDto {
 	
 	@NotEmpty
